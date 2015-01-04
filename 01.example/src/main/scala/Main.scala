f1f3771ffc8b445d40aa58e9c81e43edc3e73ad8
tomlous <tomlous@gmail.com>
2015-01-04 11:30:36 +0100

    Added solution for Example (week1)

---
+++ 01.example/src/main/scala/Main.scala
@@ -0,0 +1,5 @@
+import example.Lists
+
+object Main extends App {
+  println(Lists.max(List(1,3,2)))
+}
--- 01.example/src/main/scala/example/Lists.scala
+++ 01.example/src/main/scala/example/Lists.scala
@@ -23,7 +23,10 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = {
+    if(xs.isEmpty) 0
+    else xs.head + sum(xs.tail)
+  }
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +41,21 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = {
+
+
+    def maxLoop(curMax: Int, ls: List[Int]): Int = {
+      if(ls.isEmpty) curMax
+      else {
+        if (ls.head > curMax) {
+          maxLoop(ls.head, ls.tail)
+        } else {
+          maxLoop(curMax, ls.tail)
+        }
+      }
+    }
+
+    maxLoop(xs.head, xs.tail)
+  }
+
 }
